c5780518e099f5dd9b48674bf197d4c3f1494930
mateusseiboth <mateusseiboth@gmail.com>
2023-03-17 14:45:32 -0400

    Correção de categoria para ticket

---
+++ controllers/TicketController.php
@@ -0,0 +1,41 @@
+<?php
+  class TicketController extends Controller {
+    function editar($id) {
+      $model = new Ticket();
+      $ticket = $model->getById($id);
+      $this->view("frmTicket", compact('ticket'));
+    }
+
+    function listar() {
+      $model = new Ticket();
+      $tickets = $model->read();
+      $this->view("listarTicket", compact('tickets'));
+    }
+
+    function novo() {
+      $categoria = array();
+      $categoria['id'] = 0;
+      $categoria['descricao'] = "";
+      $this->view("frmTicket", compact('ticket'));
+    }
+
+    function salvar() {
+      $categoria = array();
+      $categoria['id'] = $_POST['id'];
+      $categoria['descricao'] = $_POST['descricao'];
+      $model = new Ticket();
+      if ($ticket['id'] == 0) {
+        $model->create($ticket);
+      } else {
+        $model->update($ticket);
+      }
+      $this->redirect('ticket/listar');
+    }
+
+    function excluir($id) {
+      $model = new Ticket();
+      $model->delete($id);
+      $this->redirect('ticket/listar');
+    }
+  }
+ ?>
