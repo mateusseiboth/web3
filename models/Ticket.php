43ed9b07df3f9355868ae7461762d57822a394d0
mateusseiboth <mateusseiboth@gmail.com>
2023-03-18 17:56:54 -0400

    Adicionada query padrão do ticket

--- models/Ticket.php
+++ models/Ticket.php
@@ -1,7 +1,17 @@
 <?php
   class Ticket extends Model {
     protected $tabela="ticket";
-    protected $ordem="descr";
+    protected $query = "SELECT to_char(ticket.hora_entrada at time zone 'UTF-4', 'HH24:MI:SS') as hora_entrada,
+    cliente.nome as nome_cliente, carro.placa as nome_placa, tipo.descr as tipo_vaga,
+    tipo.preco as preco_tipo, ticket.vaga_id as id_vaga, ticket.id as ticket_id,
+    to_char(ticket.hora_saida at time zone 'UTF-4', 'HH24:MI:SS') as hora_saida
+    FROM ticket 
+    join tipo on tipo.id = ticket.tipo_id
+    join carro on carro.id = ticket.carro_id
+    join cliente on cliente.id = carro.cliente_id
+    where ticket.estado != false";
+
+    protected $ordem="id";
 
     public function encerrar(){
       $chaves = array_keys($dados);
@@ -26,7 +36,6 @@
       }
       $sentenca->execute();
     }
-
 
     public function information($id) {
       $sql  = "SELECT to_char(ticket.hora_entrada at time zone 'UTF-4', 'HH24:MI:SS') as hora_entrada, 
