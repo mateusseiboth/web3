9b00f97b3a27c0799ea89e37dc873868410a511a
Flavio Junior <d-garm@outlook.com>
2023-03-18 19:23:15 -0400

    Refatoração dos models

--- models/Cliente.php
+++ models/Cliente.php
@@ -2,35 +2,7 @@
 class Cliente extends Model
 {
   protected $tabela = "cliente";
-  protected $query = "";
+  protected $query="select cliente.* from cliente order by id";
   protected $ordem = "id";
-
-  public function listar()
-  {
-    $this->query = "SELECT * FROM $this->tabela ORDER BY $this->ordem";
-    $resultado = $this->conexao->query($this->query);
-    return $resultado->fetchAll(PDO::FETCH_ASSOC);
-  }
-
-  public function inserir($nome, $email, $telefone)
-  {
-    $this->query = "INSERT INTO $this->tabela (nome, email, telefone) VALUES ('$nome', '$email', '$telefone')";
-    $resultado = $this->conexao->query($this->query);
-    return $resultado;
-  }
-
-  public function atualizar($id, $nome, $email, $telefone)
-  {
-    $this->query = "UPDATE $this->tabela SET nome='$nome', email='$email', telefone='$telefone' WHERE id=$id";
-    $resultado = $this->conexao->query($this->query);
-    return $resultado;
-  }
-
-  public function excluir($id)
-  {
-    $this->query = "DELETE FROM $this->tabela WHERE id=$id";
-    $resultado = $this->conexao->query($this->query);
-    return $resultado;
-  }
 }
 ?>
