daedb6d3cdbe533cb4bf85775db78a5f370017aa
mateusseiboth <mateusseiboth@gmail.com>
2023-03-18 18:00:02 -0400

    Adicionada ordem na query pois estava com erro

--- models/Tipo.php
+++ models/Tipo.php
@@ -1,7 +1,7 @@
 <?php
   class Tipo extends Model {
     protected $tabela="tipo";
-    protected $query = "SELECT tipo.* FROM tipo";
+    protected $query = "SELECT tipo.* FROM tipo order by id";
     protected $ordem="id";    
   }
 
