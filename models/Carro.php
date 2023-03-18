199003326560770c971804e2e68fdff0edd79c93
Flavio Junior <d-garm@outlook.com>
2023-03-18 18:12:14 -0400

    Adicionado métodos CRUD nas classes dos modelos Carro e Cliente

--- models/Carro.php
+++ models/Carro.php
@@ -1,7 +1,36 @@
 <?php
-  class Carro extends Model {
-    protected $tabela="carro";
-    protected $ordem="id";    
+class Carro extends Model
+{
+  protected $tabela = "carro";
+  protected $ordem = "id";
+
+  public function listar()
+  {
+    $this->query = "SELECT * FROM $this->tabela ORDER BY $this->ordem";
+    $resultado = $this->conexao->query($this->query);
+    return $resultado->fetchAll(PDO::FETCH_ASSOC);
   }
 
- ?>
+  public function inserir($nome, $email, $telefone)
+  {
+    $this->query = "INSERT INTO $this->tabela (nome, email, telefone) VALUES ('$nome', '$email', '$telefone')";
+    $resultado = $this->conexao->query($this->query);
+    return $resultado;
+  }
+
+  public function atualizar($id, $nome, $email, $telefone)
+  {
+    $this->query = "UPDATE $this->tabela SET nome='$nome', email='$email', telefone='$telefone' WHERE id=$id";
+    $resultado = $this->conexao->query($this->query);
+    return $resultado;
+  }
+
+  public function excluir($id)
+  {
+    $this->query = "DELETE FROM $this->tabela WHERE id=$id";
+    $resultado = $this->conexao->query($this->query);
+    return $resultado;
+  }
+}
+
+?>
