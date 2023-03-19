ee02d964f85be3dac7aaf43856761452d79b0063
mateusseiboth <mateusseiboth@gmail.com>
2023-03-19 08:56:24 -0400

    Adicionada verificação de usuário logado

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php
+session_start();
     include_once "autoload.php";
 
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
-
   define("APP", "http://localhost/web3/");
+
+  if (!isset($_SESSION['logado']) && $_GET['url'] != 'login/entrar') {
+    $url = 'login/login';
+  } else {
   if (isset($_GET['url'])) {
     $url = $_GET['url'];
   } else {
     $url = 'index/index';
   }
+}
   $parametros = explode("/", $url);
   $nomeControlador = ucfirst($parametros[0]).'Controller';
   
@@ -24,5 +29,3 @@
     $id = $parametros[2];
     $controlador->$acao($id);
   }
-
- 
