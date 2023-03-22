076d219c171d8705a21c1316df94f9c00bd7cbe5
Flavio Junior <d-garm@outlook.com>
2023-03-22 18:05:47 -0400

    Atualizado Query do modelo do carro

--- models/Carro.php
+++ models/Carro.php
@@ -2,7 +2,7 @@
 class Carro extends Model
 {
   protected $tabela = "carro";
-  protected $query = "SELECT carro.* FROM carro ORDER BY id";
+  protected $query = "SELECT carro.*, cliente.id as cliente_id FROM carro JOIN cliente ON carro.cliente_id = cliente.id ORDER BY id";
   protected $ordem = "id";
 }
 
