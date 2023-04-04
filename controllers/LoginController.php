47d5caaf78d976bbd0bf0514483ba2c71d590d5f
mateusseiboth <mateusseiboth@gmail.com>
2023-04-04 18:40:42 -0400

    Adição de rota de criação de usuario

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -15,6 +15,20 @@
 
     }
 
+    function cadastrar(){
+      $this->view("frmUser", array());
+    }
+
+    function novo(){
+      $user = array();
+      $user['username'] = $_POST['username'];
+      $user['password'] = $_POST['password'];
+      $user['password'] = base64_encode($user['password']);
+      $model = new Login();
+      $model->create($user);
+      $this->redirect("index/index");
+    }
+
     function deslogar(){
       session_destroy();
       $this->redirect("index/index");
