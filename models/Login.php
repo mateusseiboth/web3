9e7532bb6bd1226e0b89ab12b4f10618e824e111
mateusseiboth <mateusseiboth@gmail.com>
2023-04-07 13:21:43 -0400

    Correções no login

--- models/Login.php
+++ models/Login.php
@@ -13,9 +13,15 @@
       $sentenca->bindParam(":password", $prepared_password);
       $sentenca->execute();
       $dados = $sentenca->fetch();
+      if($dados != null || $dados != false){
         $_SESSION['username'] = $dados['username'];
         $_SESSION['id'] = $dados['id'];
         $_SESSION['logado'] = true;
+      }
+      else {
+        $_SESSION['username'] = "";
+        $_SESSION['id'] = "";
+      }
     }
     
 
